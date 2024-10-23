eb21d4d73a88e433e84ef10a613e4657d4c55caa
Simon <simoonf@vip.qq.com>
2024-10-23 16:49:40 +0800

    chore: Update demo

--- snapshots/demo-ts/index.tsx
+++ snapshots/demo-ts/index.tsx
@@ -1,7 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { SVGAttributes, FunctionComponent } from 'react';
+import React, { SVGAttributes, FunctionComponent } from 'react';
 import IconAlipay from './IconAlipay';
 import IconUser from './IconUser';
 import IconSetup from './IconSetup';
